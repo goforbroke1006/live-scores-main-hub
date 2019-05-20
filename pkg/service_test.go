df168817ba0589bc6a2fb3aa810246b5cb6f2f1c
GoForBroke <go.for.broke1006@gmail.com>
2019-05-20 14:23:44 +0300

    Detect actual deltas (improve test)

--- pkg/service_test.go
+++ pkg/service_test.go
@@ -9,49 +9,48 @@ import (
 	"testing"
 	"time"
 
+	"github.com/goforbroke1006/live-scores-main-hub/pkg/archive"
 	"github.com/goforbroke1006/live-scores-main-hub/pkg/model"
 )
 
-var writeFakeMessagesWG sync.WaitGroup
-
 type testFakeProvider struct {
-	msgCounter          uint
-	msgTotal            uint
-	teamNamesSalt       uint
-	fakeScore1          uint
-	needGoalEventsCount uint
+	msgCounter            uint
+	msgTotal              uint
+	msgWG                 *sync.WaitGroup
+	teamNamesSalt         uint
+	fakeScore1            uint
+	needGoalEventsCount   uint
+	needGoalEventsCounter uint
 }
 
 func (tfp *testFakeProvider) ReadMessage() (messageType int, p []byte, err error) {
 	if tfp.msgCounter >= tfp.msgTotal {
 		return -1, nil, errors.New("limit exceeded")
 	}
-	defer writeFakeMessagesWG.Done()
+	defer tfp.msgWG.Done()
 	tfp.msgCounter++
 	msg := fmt.Sprintf(`{"home":"fake %d","away":"fake %d","scores":{"home":%d,"away":0}}`,
 		tfp.teamNamesSalt+1,
 		tfp.teamNamesSalt+2,
 		tfp.fakeScore1,
 	)
-	if tfp.needGoalEventsCount > 0 {
+	if tfp.needGoalEventsCounter < tfp.needGoalEventsCount {
 		tfp.fakeScore1++
-		tfp.needGoalEventsCount--
+		tfp.needGoalEventsCounter++
 	}
 	return 0, []byte(msg), nil
 }
 
 func (tfp testFakeProvider) WriteJSON(v interface{}) error {
-	//panic("implement me")
 	return nil
 }
 
 func (tfp testFakeProvider) Close() error {
-	//panic("implement me")
 	return nil
 }
 
 type testFakeConsumer struct {
-	receivedMesages uint
+	receivedMessages uint
 }
 
 func (tfc testFakeConsumer) ReadMessage() (messageType int, p []byte, err error) {
@@ -59,13 +58,11 @@ func (tfc testFakeConsumer) ReadMessage() (messageType int, p []byte, err error)
 }
 
 func (tfc *testFakeConsumer) WriteJSON(v interface{}) error {
-	//panic("implement me")
-	tfc.receivedMesages++
+	tfc.receivedMessages++
 	return nil
 }
 
 func (tfc testFakeConsumer) Close() error {
-	//panic("implement me")
 	return nil
 }
 
@@ -94,42 +91,92 @@ func (fac *fakeArchiveClient) RecognizeLiveMatchEvent(team1, team2 string) uint6
 }
 
 func Test_mainHubService_Start(t *testing.T) {
-	fakeProv1ExpectedEventsCount := uint(2)
-	fakeProv2ExpectedEventsCount := uint(3)
-	fakeProviders := map[string]WebSocketConn{
-		"fake-provider-1": &testFakeProvider{msgTotal: 5, teamNamesSalt: 10,
-			fakeScore1: 0, needGoalEventsCount: fakeProv1ExpectedEventsCount},
-		"fake-provider-2": &testFakeProvider{msgTotal: 5, teamNamesSalt: 20,
-			fakeScore1: 0, needGoalEventsCount: fakeProv2ExpectedEventsCount},
-	}
-	fakeConsumers := []WebSocketConn{
-		&testFakeConsumer{},
-		&testFakeConsumer{},
-	}
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	fac := &fakeArchiveClient{matchNextID: 1, matchStorage: map[string]uint64{}}
 
-	svc := mainHubService{
-		logger:     log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
-		providers:  fakeProviders,
-		consumers:  fakeConsumers,
-		dataStream: make(chan model.Updates, 5),
-		archive:    fac,
+	type fields struct {
+		logger    *log.Logger
+		archive   archive.ArchiveService
+		providers map[string]WebSocketConn
+		consumers []WebSocketConn
 	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "Without providers",
+			fields: fields{
+				logger:    logger,
+				archive:   fac,
+				providers: map[string]WebSocketConn{},
+				consumers: []WebSocketConn{
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+				},
+			},
+		},
+		{
+			name: "Simple case",
+			fields: fields{
+				logger:  logger,
+				archive: fac,
+				providers: map[string]WebSocketConn{
+					"fake-provider-1": &testFakeProvider{msgTotal: 5, teamNamesSalt: 10,
+						fakeScore1: 0, needGoalEventsCount: 2},
+					"fake-provider-2": &testFakeProvider{msgTotal: 5, teamNamesSalt: 20,
+						fakeScore1: 0, needGoalEventsCount: 4},
+				},
+				consumers: []WebSocketConn{
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+					&testFakeConsumer{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		var writeFakeMessagesWG sync.WaitGroup
+		t.Run(tt.name, func(t *testing.T) {
+			svc := &mainHubService{
+				logger:     tt.fields.logger,
+				archive:    tt.fields.archive,
+				providers:  tt.fields.providers,
+				consumers:  tt.fields.consumers,
+				dataStream: make(chan model.Updates, 10),
+			}
 
-	writeFakeMessagesWG.Add(5 + 5)
-	go func() {
-		svc.Start()
-	}()
-	writeFakeMessagesWG.Wait()
-	time.Sleep(250 * time.Millisecond)
-	close(svc.dataStream)
+			mgsCountWait := uint(0)
+			for _, p := range tt.fields.providers {
+				p.(*testFakeProvider).msgWG = &writeFakeMessagesWG
+				mgsCountWait += p.(*testFakeProvider).msgTotal
+			}
 
-	expectedCount := uint(len(fakeProviders)) + fakeProv1ExpectedEventsCount + fakeProv2ExpectedEventsCount
+			writeFakeMessagesWG.Add(int(mgsCountWait))
+			go func() {
+				svc.Start()
+			}()
+			writeFakeMessagesWG.Wait()
+			time.Sleep(250 * time.Millisecond)
+			close(svc.dataStream)
 
-	for _, c := range fakeConsumers {
-		count := c.(*testFakeConsumer).receivedMesages
-		if count != uint(expectedCount) {
-			t.Errorf("wrong received messages count - expect %d, actual %d", expectedCount, count)
-		}
+			expectedCount := uint(len(tt.fields.providers))
+			for _, p := range tt.fields.providers {
+				tmp := p.(*testFakeProvider).needGoalEventsCount
+				expectedCount += tmp
+			}
+
+			for _, c := range tt.fields.consumers {
+				count := c.(*testFakeConsumer).receivedMessages
+				if count != uint(expectedCount) {
+					t.Errorf("wrong received messages count - expect %d, actual %d", expectedCount, count)
+				}
+			}
+		})
 	}
 }
